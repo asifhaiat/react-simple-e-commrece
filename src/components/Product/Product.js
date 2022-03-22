649b98c1396bdb07d76b43805fcb7244cd856e32
asifhaiat <asifhaiat67465@gmail.com>
2022-03-22 18:02:24 +0600

    stylr product cart and button position

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,11 +3,19 @@ import './Product.css';
 
 const Product = (props) => {
     console.log(props.product);
-    const  {name, img, seller, ratings} = props.product;
+    const  {name, img, seller, ratings, price} = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p> 
+                <p>Price: ${price}</p>
+                <p>Seller: {seller}</p>
+                <p>Ratings: {ratings} stars</p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to Cart:</p>
+            </button>
         </div>
     );
 };
